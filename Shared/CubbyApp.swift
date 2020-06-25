b2446afb1ac66769d28399b5a6040ddbbd476509
sab747 <sabrinasjain@gmail.com>
2020-06-24 20:48:14 -0700

    Initial Commit

---
+++ Shared/CubbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubbyApp.swift
+//  Shared
+//
+//  Created by Sabrina Jain on 6/24/20.
+//
+
+import SwiftUI
+
+@main
+struct CubbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
